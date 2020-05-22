9323701cc777a486a1527f225e32c51d3b76700e
liuhua <jyfc7879@gmail.com>
2020-05-22 17:21:55 +0800

    ..

---
+++ java8_in_action/src/main/java/BufferedReaderProcessor.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOError;
+import java.io.IOException;
+
+/**
+ * @author : lh
+ * @since : 2020/5/22, Fri
+ **/
+@FunctionalInterface
+public interface BufferedReaderProcessor {
+     String process(BufferedReader b) throws IOException;
+}
---
+++ java8_in_action/src/main/java/predicate/Filter.java
@@ -0,0 +1,29 @@
+package predicate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * @author : lh
+ * @since : 2020/5/22, Fri
+ **/
+public class Filter {
+    public static <T> List<T> filter(List<T> list, Predicate<T> p) {
+        List<T> results = new ArrayList<>();
+        for (T t : list) {
+            if (p.test(t)) {
+                results.add(t);
+            }
+        }
+
+        return results;
+    }
+
+    public static void main(String[] args) {
+        List<String> listOfStrings = Arrays.asList("1231", "", "2134");
+        Predicate<String> nonEmptyStringPredicate = (String s) -> !s.isEmpty();
+        List<String> nonEmpty = filter(listOfStrings, nonEmptyStringPredicate);
+    }
+}
