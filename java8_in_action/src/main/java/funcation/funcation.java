06559c8595f618e17edeb048915908b7ea7b7ee7
liuhua <jyfc7879@gmail.com>
2020-05-24 09:21:13 +0800

    ..

---
+++ java8_in_action/src/main/java/funcation/funcation.java
@@ -0,0 +1,23 @@
+package funcation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+/**
+ * @author : lh
+ * @since : 2020/5/24, Sun
+ **/
+public class funcation {
+    public static void main(String[] args) {
+        List<Integer> l = map(Arrays.asList("lambdas", "in", "action"), String::length);
+    }
+    public static <T,R> List<R> map(List<T> list, Function<T,R> f){
+        List<R> result = new ArrayList<>();
+        for (T t : list) {
+            result.add(f.apply(t));
+        }
+        return result;
+    }
+}
