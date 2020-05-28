9084b6ee702748c87e3eda9d12fdd461faa12e2d
liuhua <jyfc7879@gmail.com>
2020-05-28 15:34:09 +0800

    ..

--- java8_in_action/src/main/java/chap4/StreamTest.java
+++ java8_in_action/src/main/java/chap4/StreamTest.java
@@ -1,10 +1,10 @@
 package chap4;
 
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.Optional;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * @author : lh
@@ -12,9 +12,100 @@ import java.util.stream.Stream;
  **/
 public class StreamTest {
     public static void main(String[] args) {
-        List<String> title = Arrays.asList("Java8", "In", "Action");
-        Stream<String> s = title.stream();
-        s.forEach(System.out::println);
-        s.forEach(System.out::println);
+//        List<String> title = Arrays.asList("Java8", "In", "Action");
+//        Stream<String> s = title.stream();
+//        s.forEach(System.out::println);
+//        s.forEach(System.out::println);
+//
+//        List<Dish> vegetarianDishes = Dish.menu
+//                .stream().filter(Dish::isVegetarian)
+//                .collect(Collectors.toList());
+        //<editor-fold desc="筛选">
+        List<Integer> integers = Arrays.asList(1, 2, 1, 3, 3, 2, 4);
+        integers.stream()
+                .filter(i -> i % 2 == 0)
+                .distinct()
+                .forEach(System.out::println);
+        //</editor-fold>
+        //<editor-fold desc="截短流">
+        List<Dish> dishes = Dish.menu.stream()
+                .filter(d -> d.getCalories() > 300)
+                .limit(3)
+                .collect(toList());
+        //</editor-fold>
+        //<editor-fold desc="跳过元素">
+        List<Dish> dishes_skip = Dish.menu.stream()
+                .filter(d -> d.getCalories() > 300)
+                .skip(2)
+                .collect(toList());
+        //</editor-fold>
+
+        List<Boolean> dishNames = Dish.menu.stream().map(Dish::isVegetarian)
+                .collect(toList());
+
+
+        List<String> words = Arrays.asList("Java 8", "Lambda", "In", "Action");
+        List<Integer> wordLengths = words.stream()
+                .map(String::length)
+                .collect(toList());
+        List<Integer> dishNameLengths = Dish.menu.stream()
+                .map(Dish::getName)
+                .map(String::length)
+                .collect(toList());
+
+        words = Arrays.asList("Hello", "Word");
+        words.stream()
+                .map(word -> word.split(""))
+                .distinct().collect(toList());
+        words.stream()
+                .map(word -> word.split(""))
+                .map(Arrays::stream)
+                .distinct()
+                .collect(toList());
+
+        List<String> uniqueCharacters =
+                words.stream()
+                        .map(w -> w.split(""))
+                        .flatMap(Arrays::stream)
+                        .distinct()
+                        .collect(toList());
+
+
+        List<Integer> number1 = Arrays.asList(1, 2, 3);
+        List<Integer> number2 = Arrays.asList(3, 4);
+        List<int[]> paris = number1.stream()
+                .flatMap(i -> number2
+                        .stream()
+                        .map(j -> new int[]{i, j}))
+                .collect(toList());
+
+        List<int[]> pairs_2 = number1.stream()
+                .flatMap(i -> number2.stream()
+                        .filter(j -> (i + j) % 3 == 0)
+                        .map(j -> new int[]{i, j})
+                ).collect(toList());
+        if (Dish.menu.stream().anyMatch(Dish::isVegetarian)) {
+            System.out.println("The menu is (somewhat) vegetarian friendly!!");
+        }
+
+        boolean isHealthy = Dish.menu.stream().allMatch(d -> d.getCalories() < 1000);
+
+        isHealthy = Dish.menu.stream().noneMatch(d -> d.getCalories() >= 1000);
+
+        Optional<Dish> dish = Dish.menu.stream()
+                .filter(Dish::isVegetarian)
+                .findAny();
+
+        Dish.menu.stream()
+                .filter(Dish::isVegetarian)
+                .findAny()
+                .ifPresent(d -> System.out.println(d.getName()));
+
+        List<Integer> someNumbers = Arrays.asList(1, 2, 3, 4, 5);
+        Optional<Integer> firstSquareDivisibleByThree =
+                someNumbers.stream()
+                        .map(x -> x * x)
+                        .filter(x -> x % 3 == 0)
+                        .findFirst();
     }
 }
