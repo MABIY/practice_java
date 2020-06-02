295d29ce188f2b68f9cfb457e3cd209567bf3caf
liuhua <jyfc7879@gmail.com>
2020-06-02 16:40:32 +0800

    ..

--- java8_in_action/src/main/java/chap7/ParallelStreams.java
+++ java8_in_action/src/main/java/chap7/ParallelStreams.java
@@ -70,9 +70,37 @@ public class ParallelStreams {
 //        System.out.println("ranged sum done in:" +
 //                measureSumPerf(ParallelStreams::rangedSum, 10_000_000) + " msecs");
 
-        System.out.println("parallelRangeSum sum done in:" +
-                measureSumPerf(ParallelStreams::parallelRangeSum, 10_000_000) + " msecs");
+//        System.out.println("parallelRangeSum sum done in:" +
+//                measureSumPerf(ParallelStreams::parallelRangeSum, 10_000_000) + " msecs");
+
+
+//        System.out.println("SideEffect sum done in: " +
+//                measureSumPerf(ParallelStreams::sideEffectSum, 10_000_000L) + " msec");
+
+        System.out.println("SideEffect parallel sum done in: " +
+                measureSumPerf(ParallelStreams::sideEffectParallelSum, 10_000_000L) + " msec");
     }
 
 
+    public static long sideEffectSum(long n) {
+        Accumulator accumulator = new Accumulator();
+        LongStream.rangeClosed(1, n).forEach(accumulator::add);
+        return accumulator.total;
+    }
+
+    public static long sideEffectParallelSum(long n) {
+        Accumulator accumulator = new Accumulator();
+        LongStream.rangeClosed(1, n).parallel().forEach(accumulator::add);
+        return accumulator.total;
+    }
+
+
+}
+
+class Accumulator {
+    public long total = 0;
+
+    public void add(long value) {
+        total += value;
+    }
 }
