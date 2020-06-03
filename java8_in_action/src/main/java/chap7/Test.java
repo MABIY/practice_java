97b3688842fc1e8c9a3eb7a3d65c237d914513f0
liuhua <jyfc7879@gmail.com>
2020-06-03 18:41:53 +0800

    ..

---
+++ java8_in_action/src/main/java/chap7/Test.java
@@ -0,0 +1,18 @@
+package chap7;
+
+/**
+ * @author : lh
+ * @since : 2020/6/3, Wed
+ **/
+public class Test {
+
+    public int countWordsIteratively(String s) {
+        int counter = 0;
+        boolean lastSpace = true;
+        for (char c : s.toCharArray()) {
+            // todo
+            if (Character.isWhitespace(c)) {
+            }
+        }
+    }
+}
