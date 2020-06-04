a2ca839d2fe1002c2431bc66f50abe1751182c52
lh <jyfc7879@gmail.com>
2020-06-04 22:19:22 +0800

    ..

--- java8_in_action/src/main/java/chap7/WordCounter.java
+++ java8_in_action/src/main/java/chap7/WordCounter.java
@@ -1,5 +1,10 @@
 package chap7;
 
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static chap7.Test.SENTENCE;
+
 /**
  * @author : lh
  * @since : 2020/6/4, Thu
@@ -13,21 +18,52 @@ public class WordCounter {
         this.lastSpace = lastSpace;
     }
 
-    public WordCounter accumulate(Character c) {
+    public WordCounter  accumulate(Character c) {
         if (Character.isWhitespace(c)) {
             return lastSpace ? this :
                     new WordCounter(counter, true);
         } else {
-            return lastSpace ?
-                    new WordCounter(counter + 1, false) :
+            if (lastSpace) {
+                System.out.println(Thread.currentThread() + " accumulate : " + c);
+
+                return new WordCounter(counter + 1, false);
+            } else {
+                return this;
+            }
         }
     }
 
+    @Override
+    public String toString() {
+        return "WordCounter{" +
+                "counter=" + counter +
+                ", lastSpace=" + lastSpace +
+                '}';
+    }
+
     public WordCounter combine(WordCounter wordCounter) {
+
+        System.out.println(Thread.currentThread() + " combine : " + wordCounter);
         return new WordCounter(counter + wordCounter.counter, wordCounter.lastSpace);
     }
 
     public int getCounter() {
         return counter;
+    }
+
+    private static int countWords(Stream<Character> stream) {
+        WordCounter wordCounter = stream.parallel().reduce(new WordCounter(0, true),
+                WordCounter::accumulate,
+                WordCounter::combine
+        );
+        return wordCounter.getCounter();
+    }
+
+    public static void main(String[] args) {
+        Stream<Character> stream = IntStream.range(0, SENTENCE.length())
+                .mapToObj(SENTENCE::charAt);
+
+        System.out.println("Found " + countWords(stream) + " words");
+
     }
 }
