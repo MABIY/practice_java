6cb1ca0c71f58f7d77098fcc7a7edaad175700c7
liuhua <jyfc7879@gmail.com>
2020-06-08 10:15:39 +0800

    ..

--- java8_in_action/src/main/java/chap8/Point.java
+++ java8_in_action/src/main/java/chap8/Point.java
@@ -1,10 +1,16 @@
 package chap8;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @author : lh
  * @since : 2020/6/8, Mon
  **/
 public class Point {
+    public final static Comparator<Point> compareByXAndThenY =
+            Comparator.comparing(Point::getX).thenComparing(Point::getY);
     private final int x;
     private final int y;
 
@@ -25,6 +31,22 @@ public class Point {
         return new Point(this.x + x, this.y);
     }
 
+    public static List<Point> moveAllPointsRightBy(List<Point> points, int x) {
+        return points.parallelStream()
+                .map(p -> new Point(p.getX() + x, p.getY()))
+                .collect(Collectors.toList());
+    }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Point) {
+            Point point = (Point) obj;
+            if (x == point.getX()
+                    && y == point.getY()) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
 
--- java8_in_action/src/test/java/chap8/PointTest.java
+++ java8_in_action/src/test/java/chap8/PointTest.java
@@ -1,7 +1,12 @@
 package chap8;
 
 import junit.framework.TestCase;
+import lambda_t.predicate.Filter;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 
 /**
  * @author : lh
@@ -15,5 +20,32 @@ public class PointTest extends TestCase {
         Point p2 = p1.moveRightBy(10);
         assertEquals(15, p2.getX());
         assertEquals(5, p2.getY());
+    }
+
+    @Test
+    public void testComparingTwoPoints() throws Exception {
+        Point p1 = new Point(10, 15);
+        Point p2 = new Point(10, 20);
+
+        int result = Point.compareByXAndThenY.compare(p1, p2);
+        assertEquals(-1, result);
+    }
+
+    @Test
+    public void testMoveAllPointsRightBy() throws Exception {
+        List<Point> points = Arrays.asList(new Point(5, 5), new Point(10, 5));
+        List<Point> expectedPoints = Arrays.asList(new Point(15, 5), new Point(20, 5));
+
+        List<Point> newPoints = Point.moveAllPointsRightBy(points, 10);
+        assertEquals(expectedPoints, newPoints);
+    }
+
+    @Test
+    public void testFilter() throws Exception {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4);
+        List<Integer> even = Filter.filter(numbers, i -> i % 2 == 0);
+        List<Integer> smallerThanThree = Filter.filter(numbers, i -> i < 3);
+        assertEquals(Arrays.asList(2, 4), even);
+        assertEquals(Arrays.asList(1, 2), smallerThanThree);
     }
 }
