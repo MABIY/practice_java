6b90971ce2c3498e5929d9d5630978b1c65164d6
lh <jyfc7879@gmail.com>
2020-06-13 13:51:21 +0800

    ..

--- java8_in_action/src/main/java/chap9/B.java
+++ java8_in_action/src/main/java/chap9/B.java
@@ -4,7 +4,7 @@ package chap9;
  * @author : lh
  * @since : 2020/6/13, Sat
  **/
-public interface B extends A {
+public interface B {
 
     default void hello() {
         System.out.println("Hello from B");
--- java8_in_action/src/main/java/chap9/C.java
+++ java8_in_action/src/main/java/chap9/C.java
@@ -6,7 +6,12 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
  * @author : lh
  * @since : 2020/6/13, Sat
  **/
-public class C extends D implements A, B {
+public class C implements A, B {
+    @Override
+    public void hello() {
+        B.super.hello();
+    }
+
     public static void main(String[] args) {
         new C().hello();
     }
