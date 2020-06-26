e66e52890c37778c8a3e5bea59c7ede823c61a49
lh <jyfc7879@gmail.com>
2020-06-26 15:14:23 +0800

    ..

--- java8_in_action/src/main/java/chap14/MyList.java
+++ java8_in_action/src/main/java/chap14/MyList.java
@@ -1,5 +1,7 @@
 package chap14;
 
+import java.util.function.Supplier;
+
 /**
  * @author : lh
  * @since : 2020/6/26, Fri
@@ -53,5 +55,43 @@ class Empty<T> implements MyList<T>{
     @Override
     public MyList<T> tail() {
         throw new UnsupportedOperationException();
+    }
+}
+
+class LazyList<T> implements MyList<T>{
+    final T head;
+    final Supplier<MyList<T>> tail;
+
+    public LazyList(T head, Supplier<MyList<T>> tail) {
+        this.head = head;
+        this.tail = tail;
+    }
+
+    @Override
+    public T head() {
+        return head;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public MyList<T> tail() {
+        return tail.get();
+    }
+
+    public static LazyList<Integer> from(int n) {
+        return new LazyList<>(n, () -> from(n + 1));
+    }
+
+    public static void main(String[] args) {
+        LazyList<Integer> numbers = from(2);
+
+        int two = numbers.head;
+        int three = numbers.tail().head();
+        int five = numbers.tail().tail().tail().head();
+        System.out.println(two + " " + three + " " + five);
     }
 }
