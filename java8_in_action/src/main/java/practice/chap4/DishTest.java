b69289e43af98504b4230005fbb68a5ad2758d62
liuhua <jyfc7879@gmail.com>
2020-11-01 22:07:07 +0800

    ..

--- java8_in_action/src/main/java/practice/chap4/DishTest.java
+++ java8_in_action/src/main/java/practice/chap4/DishTest.java
@@ -1,6 +1,11 @@
 package practice.chap4;
 
+import org.omg.CORBA.INTERNAL;
+
+import javax.swing.text.html.Option;
 import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
 
 /**
  * @author lh
@@ -17,6 +22,27 @@ public class DishTest {
         }
         {
             Optional<Dish> dish = Dish.menu.stream().findAny();
+        }
+        {
+            System.out.println("total value: " + Dish.menu.stream().reduce(0,
+                    (integer, dish) -> {
+                        System.out.println("accumulator->integer:" + integer + ", dish:" + dish);
+                        return integer + dish.getCalories();
+                    },
+                    (integer, integer2) -> {
+                        System.out.println("combiner->integer:" + integer + ", integer2:" + integer2);
+                        return integer + integer2;
+
+                    }));
+        }
+        {
+            IntStream number = IntStream.range(0, 10);
+            OptionalInt max = number.reduce(Integer::max);
+        }
+
+        {
+            int count = Dish.menu.stream().map(dish -> 1).reduce(0, (a, b) -> a + b);
+            System.out.println("count value: " + count);
         }
     }
 }
